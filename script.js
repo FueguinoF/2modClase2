33ead041216c0c66338072efba5b2442b3a73556
FueguinoF <franciscoamador86@gmail.com>
2023-11-07 14:59:13 -0300

    Version entrega 1.0 - valida - estilo listo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* asociaciones al DOM */
 let op1 = document.getElementById("op1")
 let op2 = document.getElementById("op2")
 
@@ -9,14 +10,14 @@ let btdiv = document.getElementById("divbtn")
 let res = document.getElementById("ipres")
 
 
-
+/* Eventos para botones de operaciones */ 
 btsum.addEventListener("click", (a , b)=>{
     if(op1.value==""||op2.value==""){
         errorVacio()
     }else{
         let resu = parseFloat(op1.value) + parseFloat(op2.value)
         res.value = `${resu}`
-        op1.value = ""
+        op1.value = "" 
         op2.value = ""
         op1.focus()
     }
@@ -58,6 +59,10 @@ btdiv.addEventListener("click", (a , b)=>{
     }
 })
 
+/* Función de validación solo para campo vacío, se restinge el acceso al input discriminando solo por números
+y se logra una validación sencilla porque el caso lo permite, entiendo que usar restricciones desde HTML no es
+recomendado ya pero considero que es ideal en algunos casos y lo mas dinámico usarlo como primera capa de validación, 
+requiere menos uso de script*/
 function errorVacio(){
     Swal.fire({
         title: 'Error, no puede haber campos vacíos',
